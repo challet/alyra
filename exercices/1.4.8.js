5d6c262d6836102348fb1311e887f90407276c49
Clément Hallet <clement@challet.eu>
2019-04-24 23:18:16 +0200

    1.4.9

--- exercices/1.4.8.js
+++ exercices/1.4.8.js
@@ -1,6 +1,14 @@
 // https://ecole.alyra.fr/mod/assign/view.php?id=71
-
 const Hexa = require('./lib/hexa.js');
+const date_locale_config = { 
+  weekday: 'long', 
+  month: 'long', 
+  year: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric'
+};
 
 var quatre_octets = new Hexa(Buffer.alloc(4, 0xff));
 var valeur_max = quatre_octets.toNumber();
@@ -12,13 +20,5 @@ var timestamp = new Date(0);
 // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date/setSeconds#Description
 timestamp.setSeconds(valeur_max);
 
-var temps_lisible = timestamp.toLocaleString('fr-FR' , { 
-  weekday: 'long', 
-  month: 'long', 
-  year: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric'
-});
+var temps_lisible = timestamp.toLocaleString('fr-FR' , date_locale_config);
 console.log("Un timestamp codé sur 4 octets atteindra sa limite le :", temps_lisible);
