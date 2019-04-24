a9b7e91ee5fc1ad89e6fc372afc6f18789860257
Clément Hallet <clement@challet.eu>
2019-04-24 23:22:32 +0200

    relecture

--- exercices/1.4.9.js
+++ exercices/1.4.9.js
@@ -11,20 +11,17 @@ const date_locale_config = {
   timeZone: 'UTC'
 };
 
-const mo = 1024 * 1024
-
-const dix_minutes = 60 * 1000; // en ms
+const dix_minutes = 10 * 60 * 1000; // en ms
 const taille_par_bloc = 80;
+const mo = 1024 * 1024;
 
 // timestamp 1231006505 : GMT Saturday 3 January 2009 18:15:05
 // L'objet Date prend en argument le nombre de ms
 var timestamp = new Date(1231006505 * 1000);
 var maintenant = new Date(Date.now());
-
 var nb_blocs = Math.ceil((maintenant - timestamp) / dix_minutes);
-
 var poids = nb_blocs * taille_par_bloc;
 
-console.log(`Entre le ${timestamp.toLocaleString('fr-FR', date_locale_config)} et le ${maintenant.toLocaleString('fr-FR', date_locale_config)}, ${nb_blocs.toLocaleString('fr-FR')} blocs ont été créés. La taille totale des en-têtes est ${(poids / mo).toLocaleString('fr-FR')} Mo.`)
-
-
+console.log(
+`Entre le ${timestamp.toLocaleString('fr-FR', date_locale_config)} et le ${maintenant.toLocaleString('fr-FR', date_locale_config)}, ${nb_blocs.toLocaleString('fr-FR')} blocs ont été créés. La taille totale des en-têtes est ${(poids / mo).toLocaleString('fr-FR')} Mo.`
+);
