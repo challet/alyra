32f88c85b5eed7845958f28be06a8d44b6b76e79
Clément Hallet <clement@challet.eu>
2019-05-22 17:02:05 +0200

    exercice 3.1.3

---
+++ exercices/3.1.3.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.7;
+contract Assemblee {
+    address[] membres;
+    string[] descriptionDecisions;
+    uint[] votesPour;
+    uint[] votesContre;
+    
+    function rejoindre() public {
+        membres.push(msg.sender);
+    }
+ 
+    function estMembre(address utilisateur) public view returns (bool) {
+        for (uint i=0; i < membres.length; i++) {
+            if (membres[i] == utilisateur) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function proposerDecision(string memory description) public {
+        if (estMembre(msg.sender)) {
+            descriptionDecisions.push(description);
+            votesPour.push(0);
+            votesContre.push(0);
+        }
+    }
+    
+    function voter(uint vote, bool sens) public {
+        if (estMembre(msg.sender)) {
+            if (sens) {
+                votesPour[vote]++;
+            } else {
+                votesContre[vote]++;
+            }
+        }
+    }
+
+    function comptabiliser(uint indice) public view returns (int) {
+        return int(votesPour[vote]) - int(votesContre[vote]);
+    }
+    
+}
